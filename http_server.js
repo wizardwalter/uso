b6e4d46c414b370ec2ad8292521e0e8b0e0e2804
Walter Vannoy <waldwarf@gmail.com>
2021-09-29 00:44:40 -0400

    ready to deploy

--- http_server.js
+++ http_server.js
@@ -48,6 +48,6 @@ app.post('/add', function(req, res){
 
 // start server
 // -----------------------
-app.listen(3000, function(){
-    console.log('Running on port 3000!')
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Running!')
 })
